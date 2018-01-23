e2a8f66d207a8c0b8c08dc43a93547cf3ac652f5
benjamin <dumstick228@gmail.com>
2018-01-23 15:06:21 +0100

    post risky facebook post

--- munnycore/settings.py
+++ munnycore/settings.py
@@ -167,6 +167,8 @@ SOCIALACCOUNT_PROVIDERS = {
 }
 SITE_ID = 1
 
+LOGIN_REDIRECT_URL="/"
+
 # Heroku: Update database configuration from $DATABASE_URL.
 import dj_database_url
 db_from_env = dj_database_url.config(conn_max_age=500)
