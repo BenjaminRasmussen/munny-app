a1feb5d611ef744d5677c5b241a5b7f9da9d3084
benjamin <dumstick228@gmail.com>
2018-01-23 14:48:01 +0100

    Risky facebook commit

--- munnycore/settings.py
+++ munnycore/settings.py
@@ -138,6 +138,33 @@ USE_TZ = True
 FACEBOOK_APP_ID = str('186028088657155')
 FACEBOOK_APP_SECRET = str('e6f5b9c45de16fb3e9f572a8a52c724d')
 
+
+SOCIALACCOUNT_PROVIDERS = {
+    'facebook': {
+        'METHOD': 'oauth2',
+        'SCOPE': ['email', 'public_profile', 'user_friends'],
+        'AUTH_PARAMS': {'auth_type': 'reauthenticate'},
+        'INIT_PARAMS': {'cookie': True},
+        'FIELDS': [
+            'id',
+            'email',
+            'name',
+            'first_name',
+            'last_name',
+            'verified',
+            'locale',
+            'timezone',
+            'link',
+            'gender',
+            'updated_time',
+            'photo',
+        ],
+        'EXCHANGE_TOKEN': True,
+        'LOCALE_FUNC': 'path.to.callable',
+        'VERIFIED_EMAIL': False,
+        'VERSION': 'v2.5',
+    }
+}
 SITE_ID = 1
 
 # Heroku: Update database configuration from $DATABASE_URL.
