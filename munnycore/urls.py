e0201858e35b5c19d2aa9a2c328c8dab68acee6d
benjamin <dumstick228@gmail.com>
2018-01-23 11:04:20 +0100

    mesasm

--- munnycore/urls.py
+++ munnycore/urls.py
@@ -16,9 +16,6 @@ Including another URLconf
 from django.conf.urls import url, include
 from django.contrib import admin
 from django.views.generic import RedirectView
-from django.contrib.auth import views as auth_views
-
-
 from src import views
 
 urlpatterns = [
