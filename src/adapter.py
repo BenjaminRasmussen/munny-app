4baa7a9a47f6475c724d461e09598c5d15f627d4
benjamin <dumstick228@gmail.com>
2018-01-27 20:40:57 +0100

    Risky deploy #2

--- src/adapter.py
+++ src/adapter.py
@@ -4,6 +4,7 @@ from django.shortcuts import HttpResponse,redirect
 from allauth.account.models import EmailAddress
 from allauth.exceptions import ImmediateHttpResponse
 from allauth.socialaccount.signals import social_account_added
+
 from allauth.account.utils import perform_login
 from django.dispatch import receiver
 from allauth.socialaccount.adapter import DefaultSocialAccountAdapter
@@ -13,9 +14,10 @@ from src.models import munnyuser
 
 @receiver(social_account_added)
 def link_to_local_user(sender, request, sociallogin, **kwargs):
+    print(sociallogin)
     try:
         userid = request.session.get('munnyid')
         muser = munnyuser.objects.get(MUNID=userid)
-        muser.FACEBOOKLINK = sociallogin.account
+        muser.FACEBOOKLINK = sociallogin
     except:
         pass
