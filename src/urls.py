ed263dd459fbbac35bffc0ecb0eeb59019cc178c
benjamin <dumstick228@gmail.com>
2018-01-27 17:03:33 +0100

    Risky deploy #2

--- src/urls.py
+++ src/urls.py
@@ -9,16 +9,20 @@ urlpatterns = [
     # Maps the empty url (index) to the index page
     url(r'^$', views.index, name='index'),
     url(r'^admin/', admin.site.urls),
+
+    # Content pages from nav bar
     url(r'^fruit/', views.fruitleader, name='fruit'),
     url(r'^fruitleader/', views.fruitleader, name='fruitleader'),
     url(r'^ticketview/', views.ticketview, name='ticketview'),
     url(r'^sessions/', views.sessionsview, name='sessions'),
     url(r'^missingloginpage/', views.missingloginpage, name="missingloginpage"),
     url(r'^friendfinder/', views.friendfinderview, name="friendfinderview"),
-
     url(r'^suc/', views.suc, name="suc"),
+
+    # Legal pages
     url(r'^credits/', views.creditsview, name="credits"),
     url(r'^PrivacyPolicy/', views.PricavyPolicyview, name="PrivacyPolicy"),
     url(r'^TOS', views.TOSview, name="TOS"),
+
 
 ]
