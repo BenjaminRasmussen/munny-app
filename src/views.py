f208910cb92a03dc7de329e062b0a1c3684ea5e5
benjamin <dumstick228@gmail.com>
2018-01-29 05:52:46 +0100

    Icon Deploy

--- src/views.py
+++ src/views.py
@@ -202,7 +202,7 @@ def friendfinderview(request):
 
     # Get number of mutual matches
     confrimedmatches = []
-    matchlist = list(friendfindermatch.objects.all().values("matcher"))
+    matchlist = list(friendfindermatch.objects.all().values("matcher")['matcher'])
     for i in matchlist:
         try:
             if friendfindermatch.objects.get(matcher=i, matchee=currentsocialaccount.uid):
