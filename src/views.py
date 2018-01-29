e1d8c9f841b56a13db4db9f14e4328b2b775db88
benjamin <dumstick228@gmail.com>
2018-01-29 07:08:20 +0100

    Icon Deploy

--- src/views.py
+++ src/views.py
@@ -402,3 +402,33 @@ def ajaxcallview(request):
         response_data['message'] = "The subprocess module did not run the script correctly!"
 
     return HttpResponse(json.dumps(response_data), content_type="application/json")
+
+
+# When account is created via social, fire django-allauth signal to populate Django User record.
+from allauth.account.signals import user_signed_up
+from django.dispatch import receiver
+
+
+@receiver(user_signed_up)
+def user_signed_up_(request, user, sociallogin=None, **kwargs):
+    '''
+    When a social account is created successfully and this signal is received,
+    django-allauth passes in the sociallogin param, giving access to metadata on the remote account, e.g.:
+
+    sociallogin.account.provider  # e.g. 'twitter'
+    sociallogin.account.get_avatar_url()
+    sociallogin.account.get_profile_url()
+    sociallogin.account.extra_data['screen_name']
+
+    See the socialaccount_socialaccount table for more in the 'extra_data' field.
+    '''
+    if sociallogin:
+        # Extract first / last names from social nets and store on User record
+
+
+        if sociallogin.account.provider == 'facebook':
+            user.first_name = sociallogin.account.extra_data['first_name']
+            user.last_name = sociallogin.account.extra_data['last_name']
+
+
+        user.save()
